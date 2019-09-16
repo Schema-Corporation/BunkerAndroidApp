8c1ce927effd578c6cd1680e54305b7565a20ec4
Drassleon <toconasalvaro@gmail.com>
2019-09-15 19:44:41 -0500

    OnBoarding Activity 80% - Missing Images
    Signed-off-by: Drassleon <toconasalvaro@gmail.com>

--- app/src/main/java/pe/edu/upc/bunker/modelViews/OnBoardingActivity.kt
+++ app/src/main/java/pe/edu/upc/bunker/modelViews/OnBoardingActivity.kt
@@ -1,13 +1,30 @@
 package pe.edu.upc.bunker.modelViews
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import pe.edu.upc.bunker.R
 
 class OnBoardingActivity : AppCompatActivity() {
 
+    lateinit var startButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
+        startButton = findViewById(R.id.startButton)
+        pressButton()
+    }
+    private fun pressButton() {
+        startButton.setOnClickListener {
+            val sharedPref: SharedPreferences = getSharedPreferences("FirstRun", Context.MODE_PRIVATE)
+            sharedPref.edit().putBoolean("FirstRun",false).apply()
+            val loginIntent = Intent(this,MainActivity::class.java)
+            startActivity(loginIntent)
+            finish()
+        }
     }
 }
