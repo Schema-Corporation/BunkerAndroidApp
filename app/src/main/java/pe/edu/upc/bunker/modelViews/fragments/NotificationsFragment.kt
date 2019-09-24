aa8900a3f322d9180d9cf7f91f361ce97d434504
Marcelo Rios <u201521895@upc.edu.pe>
2019-09-23 19:48:12 -0500

    Floating navigation button added

--- app/src/main/java/pe/edu/upc/bunker/modelViews/fragments/NotificationsFragment.kt
+++ app/src/main/java/pe/edu/upc/bunker/modelViews/fragments/NotificationsFragment.kt
@@ -13,7 +13,7 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view  = inflater.inflate(R.layout.fragment_home,container,false)
+        val view  = inflater.inflate(R.layout.fragment_notifications,container,false)
 
         return view
     }
