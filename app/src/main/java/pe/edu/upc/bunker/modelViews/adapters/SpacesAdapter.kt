0ac5e3cac9c099ceeff5ffa0af9e60e1165b283d
Drassleon <toconasalvaro@gmail.com>
2019-09-26 16:35:10 -0500

    Fix for manifest, Firebase Configured Firebase and fixed no tag on manifest
    Signed-off-by: Drassleon <toconasalvaro@gmail.com>

--- app/src/main/java/pe/edu/upc/bunker/modelViews/adapters/SpacesAdapter.kt
+++ app/src/main/java/pe/edu/upc/bunker/modelViews/adapters/SpacesAdapter.kt
@@ -1,8 +1,20 @@
 package pe.edu.upc.bunker.modelViews.adapters
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import pe.edu.upc.bunker.models.Space
 
 class SpacesAdapter (var list: List<Space>): RecyclerView.Adapter<SpacesViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SpacesViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: SpacesViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
 }
