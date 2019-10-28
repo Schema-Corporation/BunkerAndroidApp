bb89ae73f539ad8aca8409d5754bd9f05f6cc2e8
Drassleon <toconasalvaro@gmail.com>
2019-10-27 20:10:58 -0500

    Fixes and Interface improvement for step 1
    Fixed default value of zero for space type
    Added units for price
    Added functions for length validation of numeric fields
    Added dynamic checking for text length
    
    Signed-off-by: drassleon <toconasalvaro@gmail.com>

--- app/src/main/java/pe/edu/upc/bunker/modelViews/activities/CreateSpaceStepOneActivity.kt
+++ app/src/main/java/pe/edu/upc/bunker/modelViews/activities/CreateSpaceStepOneActivity.kt
@@ -4,11 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
-import android.text.Spannable
-import android.text.SpannableStringBuilder
 import android.text.TextWatcher
-import android.text.style.RelativeSizeSpan
-import android.text.style.SuperscriptSpan
 import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
@@ -28,7 +24,6 @@ import retrofit2.Response
 
 
 class CreateSpaceStepOneActivity : AppCompatActivity() {
-    private var unitsAddedFlag: Boolean = true
     private lateinit var nextStepButton: Button
 
     private lateinit var widthEditText: TextInputEditText
@@ -63,9 +58,10 @@ class CreateSpaceStepOneActivity : AppCompatActivity() {
         titleEditText = findViewById(R.id.input_title_edit_text)
         priceEditText = findViewById(R.id.price_text_input_edit)
         nextStepButton = findViewById(R.id.step_2_next_button)
-        //keyPressedLength(widthEditText, 3)
-        //keyPressedLength(heightEditText, 3)
-        //keyPressedLength(areaEditText, 4)
+        keyPressedLength(widthEditText, 3)
+        keyPressedLength(heightEditText, 3)
+        keyPressedLength(areaEditText, 4)
+        addingCurrency("\$", 5)
         pressNextStepButton()
     }
 
@@ -73,39 +69,36 @@ class CreateSpaceStepOneActivity : AppCompatActivity() {
         nextStepButton.setOnClickListener {
 
             val width = widthEditText.text.toString()
-            text_input_width.error = null
-            if (width.isEmpty()) {
-                text_input_width.error = "Este campo es requerido"
-            } else if (width.toInt() < 0) {
-                text_input_width.error = "Este campo no puede ser negativo"
+            when {
+                width.isEmpty() -> text_input_width.error = "Este campo es requerido"
+                width.toInt() < 0 -> text_input_width.error = "Este campo no puede ser negativo"
+                else -> text_input_width.error = null
             }
             spaceTypeTextView = findViewById(R.id.space_type_dropdown)
-            var spaceType = 0
+            var spaceType = 2
             when (spaceTypeTextView.text.toString()) {
                 "Oficina" -> spaceType = 0
                 "Espacio de trabajo" -> spaceType = 1
                 "Almacén" -> spaceType = 2
             }
             val height = heightEditText.text.toString()
-            text_input_height.error = null
-            if (height.isEmpty()) {
-                text_input_height.error = "Este campo es requerido"
-            } else if (height.toInt() < 0) {
-                text_input_height.error = "Este campo no puede ser negativo"
+            when {
+                height.isEmpty() -> text_input_height.error = "Este campo es requerido"
+                height.toInt() < 0 -> text_input_height.error = "Este campo no puede ser negativo"
+                else -> text_input_height.error = null
             }
             val area = areaEditText.text.toString()
-            text_input_area.error = null
-            if (area.isEmpty()) {
-                text_input_area.error = "Este campo es requerido"
-            } else if (area.toInt() < 0) {
-                text_input_area.error = "Este campo no puede ser negativo"
+            when {
+                area.isEmpty() -> text_input_area.error = "Este campo es requerido"
+                area.toInt() < 0 -> text_input_area.error = "Este campo no puede ser negativo"
+                else -> text_input_area.error = null
             }
             val title = titleEditText.text.toString()
             if (title.isEmpty()) {
                 text_input_title.error = "Este campo es requerido"
             }
             priceEditText.error = null
-            val price = priceEditText.text.toString()
+            val price = priceEditText.text.toString().split("\$")[1]
             if (price.isEmpty()) {
                 price_input_layout.error = "Este campo es requerido"
             }
@@ -113,13 +106,17 @@ class CreateSpaceStepOneActivity : AppCompatActivity() {
                 text_input_height.error.isNullOrEmpty() &&
                 text_input_area.error.isNullOrEmpty() &&
                 text_input_title.error.isNullOrEmpty() &&
-                price_input_layout.error.isNullOrEmpty()
+                price_input_layout.error.isNullOrEmpty() &&
+                areaEditText.error.isNullOrEmpty() &&
+                widthEditText.error.isNullOrEmpty() &&
+                heightEditText.error.isNullOrEmpty() &&
+                priceEditText.error.isNullOrEmpty()
             ) {
                 dbHandler.startSpace()
 
                 val sharedPreferences = this@CreateSpaceStepOneActivity.getSharedPreferences(
                     "Login",
-                            Context.MODE_PRIVATE
+                    Context.MODE_PRIVATE
                 )
                 val userId = sharedPreferences.getInt("UserId", 0)
                 val token = sharedPreferences.getString("Token", "test")
@@ -135,11 +132,6 @@ class CreateSpaceStepOneActivity : AppCompatActivity() {
 
                         override fun onResponse(call: Call<Lessor>, response: Response<Lessor>) {
                             Log.d("NetworkingDebug", call.request().body().toString())
-
-                            //height.split("M2")
-                            //width.split("M2")
-                            //area.split("M2")
-
                             val body = response.body()
                             val lessorId = body!!.id
                             dbHandler.addFirstStep(
@@ -167,37 +159,22 @@ class CreateSpaceStepOneActivity : AppCompatActivity() {
                             startActivity(nextStepIntent)
                             finish()
                         }
-
                     })
             }
         }
     }
 
     private fun keyPressedLength(editTextView: EditText, fieldLength: Int) {
-        var originalText: String
         editTextView.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(p0: Editable?) {
                 editTextView.removeTextChangedListener(this)
                 if (p0 != null) {
-                    Log.d("TextDebug", p0.toString())
-                    val textSplit = p0.split("M2")
-                    originalText = when {
-                        textSplit.size > 1 -> textSplit[0] + textSplit[1]
-                        textSplit.isEmpty() -> ""
-                        textSplit.size == 1 && textSplit[0].contains("M") -> {
-                            textSplit[0].split("M")[0].substring(0..textSplit[0].split("M")[0].length - 2)
-                        }
-                        else -> textSplit[0]
-                    }
-
-                    if (p0.length > fieldLength + 2) {
+                    if (p0.length > fieldLength) {
                         editTextView.error =
                             "Se acepta un máximo de $fieldLength dígitos para este campo"
                     } else {
                         editTextView.error = null
                     }
-                    val newText = originalText + createSuperScript()
-                    p0.replace(0, p0.length, newText)
                     if (p0.isEmpty()) {
                         return
                     }
@@ -214,12 +191,27 @@ class CreateSpaceStepOneActivity : AppCompatActivity() {
         })
     }
 
-    private fun createSuperScript(): SpannableStringBuilder {
-        val cs = SpannableStringBuilder("M2")
-        cs.setSpan(SuperscriptSpan(), 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        cs.setSpan(RelativeSizeSpan(0.25f), 1, 2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        return cs
-    }
+    private fun addingCurrency(currency: String, maxLength: Int) {
+        priceEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                priceEditText.removeTextChangedListener(this)
+                if (s != null && s.length == 1 && s[0] != currency[0])
+                    s.replace(0, s.length, "$currency${s[0]}")
+                if (s != null && s.length > maxLength + 1) {
+                    priceEditText.error =
+                        "Se acepta un máximo de $maxLength dígitos para este campo"
+                } else {
+                    priceEditText.error = null
+                }
+                priceEditText.addTextChangedListener(this)
+            }
 
-    fun String.toEditable(): Editable = Editable.Factory.getInstance().newEditable(this)
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+        })
+    }
 }
